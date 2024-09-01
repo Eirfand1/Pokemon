e7c6c01948ab9f0d7c4e54917ddc3e3e194d8022
Eirfand1 <name.fandi07@proton.me>
2024-09-01 17:57:04 +0700

    feat: detail & pagination

--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react'
-import { Card } from 'flowbite-react';
+import { Card, Button, Modal } from 'flowbite-react';
 
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20); 
+  const [modal, setModal] = useState(false);
+  const [pokeDetail, setPokeDetail] = useState([]);
 
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
-        const url = 'https://pokeapi.co/api/v2/pokemon?limit=21';
-        const response = await fetch(url);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
         const jsonData = await response.json();
         const pokemonDetails = await Promise.all(
           jsonData.results.map(async (item) => {
@@ -27,7 +30,39 @@ const Pokemon = () => {
       }
     };
     fetchPokemon();
-  }, []);
+  }, [offset]);
+
+  const nextPage = ()=> {
+    setLoading(true);
+    setOffset(offset=>offset + limit);
+  } 
+
+  const prevPage = ()=> {
+    setLoading(true);
+    offset === 0 ? setLoading(false) : setOffset(offset=>offset - limit);
+
+  }
+
+  const PokemonCard = ({ result }) => {
+    const img = result.sprites.front_default;
+
+    return (
+      <Card key={result.id} className="w-full max-w-sm border-4 border-purple-300" onClick={()=>{
+        setModal(true);
+        setPokeDetail(result);
+      }}>
+          <h2 className="text-l rounded p-4 bg-amber-100 w-full text-red-600 font-bold capitalize text-center">{result.name}</h2>
+          <img 
+            src={img} 
+            alt={result.name} 
+            className="w-48 mx-auto transition-all duration-300"
+          />
+          <p className="text-center">Type: {result.types.map(type => type.type.name).join(', ')}</p>
+      </Card>
+    );
+  };
+
+  
 
 
   if (loading) return( 
@@ -39,24 +74,40 @@ const Pokemon = () => {
   if (error) return <div className="flex justify-center items-center h-screen">{error}</div>;
 
   return (
+
     <div className="bg-pink-100 min-h-screen py-8 font-roboto">
       <h1 className="sm:text-3xl text-2xl font-bold text-center mb-10 font-pressStart">MON MON POKEMON WS KEDEKEP MON</h1>
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+      <div className="container w-10/12 mx-auto px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-items-center">
           {pokemon.map((result) => (
-            <Card key={result.id} className="w-full max-w-sm border-4 border-purple-300">
-              <div className="p-4">
-                <h2 className="text-xl rounded p-4 bg-amber-100 w-full text-red-600 font-bold capitalize text-center">{result.name}</h2>
-                  <img src={result.sprites.front_default} alt={result.name} className="w-48 mx-auto" />
-                <p className="text-center">Type: {result.types.map(type => type.type.name).join(', ')}</p>
-              </div>
-            </Card>
+            <PokemonCard key={result.id} result={result}/> 
           ))}
+        </div>  
+        <div className='flex py-5 justify-between font-pressStart'>
+          <div><Button color="light" className='border-4' onClick={prevPage}>Prev</Button></div>
+          <div><Button color="light" className='border-4' onClick={nextPage}>Next</Button></div>
         </div>
       </div>
       <style jsx global>
           @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Roboto:wght@400;700&display=swap');
       </style>
+      {
+        modal && (
+          <Modal show={modal} onClose={() => setModal(false)} popup>
+            <Modal.Header/>
+            <Modal.Body className="flex flex-col gap-8 justify-center items-center ">
+              <h1 className="text-3xl font-bold uppercase font-pressStart">{pokeDetail.name}</h1>
+              <img src={pokeDetail.sprites.other.dream_world.front_default} alt="poke" className='w-60' /> 
+              {console.log(pokeDetail)}
+              <div>
+
+              </div>
+            </Modal.Body>
+          </Modal>
+         
+        )
+      }
+      
     </div>
 
   )
